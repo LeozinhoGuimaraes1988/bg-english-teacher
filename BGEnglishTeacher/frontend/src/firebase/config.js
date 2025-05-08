78fd8157717184b16f0e37eb0b34ca0d809701b7
Leonardo Guimarães <94874711+LeozinhoGuimaraes1988@users.noreply.github.com>
2025-05-08 16:11:22 -0300

    Update config.js

--- BGEnglishTeacher/frontend/src/firebase/config.js
+++ BGEnglishTeacher/frontend/src/firebase/config.js
@@ -4,14 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDsfJJzoEq3npO3pqVGp1fJvfMxvYhBXAI',
-  authDomain: 'bg-english-teacher.firebaseapp.com',
-  projectId: 'bg-english-teacher',
-  storageBucket: 'bg-english-teacher.appspot.com',
-  messagingSenderId: '990029545053',
-  appId: '1:990029545053:web:c22400633c5ecffdb1d7b4',
-};
+
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
